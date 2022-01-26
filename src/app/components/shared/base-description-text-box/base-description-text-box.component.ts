d9e1acfe5964e131a6430fe6a4a004f5518a37fe
Isaac Ludlow <isaac@theludlows.org>
2022-01-25 18:58:28 -0700

    [DLR-3] Updated component file scructure to match new pattern.

--- src/app/components/shared/base-description-text-box/base-description-text-box.component.ts
+++ src/app/components/shared/base-description-text-box/base-description-text-box.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 
 @Component({
   selector: 'base-description-text-box',
-  templateUrl: './base-description-text-box.component.html',
+  template: `
+    <div [ngClass]="[textSize, textColor]">
+      <ng-content></ng-content>
+    </div>
+  `,
   styleUrls: ['./base-description-text-box.component.scss']
 })
 export class BaseDescriptionTextBoxComponent implements OnInit {
