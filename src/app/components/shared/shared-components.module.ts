91f797c238982896292c5910b6d9fd742741ce69
Isaac Ludlow <isaac@theludlows.org>
2022-01-26 19:23:45 -0700

    [DLR-6] Changes to the home component to preview the container cover image.

--- src/app/components/shared/shared-components.module.ts
+++ src/app/components/shared/shared-components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseIconComponent } from './base-icon/base-icon.component';
 import { IonicModule } from '@ionic/angular';
+import { ContainerImageCoverComponent } from './container-image-cover/container-image-cover.component';
 
 
 
 @NgModule({
   declarations: [
-    BaseIconComponent
+    BaseIconComponent,
+    ContainerImageCoverComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    BaseIconComponent
+    BaseIconComponent,
+    ContainerImageCoverComponent
   ]
 })
 export class SharedComponentsModule { }
