5b09486e2eb5b5c718694855b7f063aaf8a528d6
Isaac Ludlow <isaac@theludlows.org>
2022-02-09 21:31:21 -0700

    [DLR-8] Updating profile icon component to have small and large sizes.

--- src/app/components/shared/profile-preview-icons/profile-preview-icons.component.ts
+++ src/app/components/shared/profile-preview-icons/profile-preview-icons.component.ts
@@ -4,11 +4,12 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'profile-preview-icons',
   template: `
     <div class="profile-icons-container">
-      <img class="profile-images" *ngFor="let profileImageUrl of profileImageUrls" [src]="profileImageUrl">
+      <img class="profile-images" [ngClass]="profileIconSize" *ngFor="let profileImageUrl of profileImageUrls" [src]="profileImageUrl">
     </div>
   `,
   styleUrls: ['./profile-preview-icons.component.scss']
 })
 export class ProfilePreviewIconsComponent {
   @Input() profileImageUrls: Array<string>;
+  @Input() profileIconSize: string;
 }
