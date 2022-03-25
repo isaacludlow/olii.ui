3ff6907c1569c3c4178798723a3c44ad34d36888
Isaac Ludlow <isaac@theludlows.org>
2022-03-25 10:39:08 -0600

    [DLR-13] Added profile dto.

---
+++ src/app/models/dto/profile/profile.dto.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    ProfileId: number;
+    UserId: number;
+    NumberOfConnections: number;
+    Bio: string;
+    HomeCountry: string;
+    HostCountry: string;
+    HostCity: string;
+    ProfileImageUrl: string;
+}
