956699da77af1601047efd49e6249ce63617cbf8
Isaac Ludlow <isaac@theludlows.org>
2022-03-27 13:41:13 -0600

    [DLR-13] Adding comments to dtos.

--- src/app/models/dto/profile/profile-image.dto.ts
+++ src/app/models/dto/profile/profile-image.dto.ts
@@ -1,4 +1,6 @@
 export interface ProfileImage {
+    /** Id for placement and/or order on the profile page. */
     Id: number;
+    /** Urls to images. */
     Url: string;
 }
--- src/app/models/dto/profile/profile.dto.ts
+++ src/app/models/dto/profile/profile.dto.ts
@@ -1,13 +1,22 @@
 import { ProfileImage } from "./profile-image.dto";
 
 export interface Profile {
+    /** Id of the profile. */
     ProfileId: number;
+    /** Id of the user. */
     UserId: number;
+    /** Number of connections the user has. */
     NumberOfConnections: number;
+    /** Bio for the profile. */
     Bio: string;
+    /** Country user is from. */
     HomeCountry: string;
+    /** Country user is studying abroad in. */
     HostCountry: string;
+    /** City user is studying abroad in. */
     HostCity: string;
+    /** Profile picture url of the user. Shown in thumbnails on events, groups, etc.  */
     ProfilePictureUrl: string;
+    /** Profile images on the user's profile to show who they are. */
     ProfileImages: ProfileImage[];
 }
