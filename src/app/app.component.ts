64e5e5083382fe10f7f9f0da4569992c2fa095b0
Isaac Ludlow <isaac@theludlows.org>
2022-05-19 21:30:18 -0600

    [DLR-16] Adding functionality to hide the nav bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavBarService } from './shared/services/nav-bar/nav-bar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  hideNavBar: boolean;
+
+  constructor(private navBarService: NavBarService) {}
+
+  ngOnInit(): void {
+    this.navBarService.navBarVisibility.subscribe(currentVisibility => this.hideNavBar = !currentVisibility);
+    this.navBarService.setNavBarVisibility(false);
+  }
 }
---
+++ src/app/shared/services/nav-bar/nav-bar.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+
+import { NavBarService } from './nav-bar.service';
+
+describe('NavBarService', () => {
+  let service: NavBarService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(NavBarService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getter for the current navBarVisibility value', () => {
+    it('should return the current navBarVisibility', () => {
+      service.navBarVisibility.next(false);
+
+      const currentValue = service.navBarVisibilityValue;
+
+      expect(currentValue).toBeTruthy();
+    });
+  });
+
+  describe('setNavBarVisibility', () => {
+    it('should set the navBarVisibility', () => {
+      service.navBarVisibility.next(false);
+
+      service.setNavBarVisibility(true);
+
+      expect(service.navBarVisibility.value).toBe(true);
+    });
+  });
+});
