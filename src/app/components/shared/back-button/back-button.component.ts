111a7170b158fbb3cd58f4e6afa7b55f79d71a10
Isaac Ludlow <isaac@theludlows.org>
2022-05-23 11:30:50 -0600

    [DLR-17] Added back button component.

---
+++ src/app/components/shared/back-button/back-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'olii-back-button',
+  template: `
+    <div>
+      <olii-base-icon name="arrow-back" color="primary"></olii-base-icon>
+    </div>
+  `,
+  styleUrls: ['./back-button.component.scss']
+})
+export class BackButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
