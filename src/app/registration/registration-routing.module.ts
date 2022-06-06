2ee751e0976c352544311a1529d60d0cffbead2d
Isaac Ludlow <isaac@theludlows.org>
2022-06-06 17:41:12 -0600

    [DLR-17] Registration flow, base pages.

--- src/app/registration/registration-routing.module.ts
+++ src/app/registration/registration-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationFlowPage } from './pages/registration-flow/registration-flow.page';
 import { RegistrationSlideshowPage } from './pages/registration-slideshow/registration-slideshow.page';
 import { RegistrationPage } from './registration.page';
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: RegistrationPage
+  },
+  {
+    path: 'registration-flow',
+    component: RegistrationFlowPage
   },
   {
     path: 'slideshow',
