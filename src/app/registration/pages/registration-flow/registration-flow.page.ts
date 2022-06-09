1e87cae80c6362fae0494b80aae3c2a62e5f99ee
Isaac Ludlow <isaac@theludlows.org>
2022-06-08 22:55:25 -0600

    [DLR-17] Adding form fields to registration flow form.

--- src/app/registration/pages/registration-flow/registration-flow.page.ts
+++ src/app/registration/pages/registration-flow/registration-flow.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { IonSlides } from '@ionic/angular';
+import { AutocompleteResponseFields, AutocompleteResponseTypes } from 'src/app/models/dto/google-maps/autocomplete-types.dto';
 
 @Component({
   templateUrl: './registration-flow.page.html',
@@ -8,6 +10,15 @@ import { IonSlides } from '@ionic/angular';
 export class RegistrationFlowPage {
   @ViewChild('slider') slides: IonSlides;
   slideOptions = { initialSlide: 0, speed: 400, allowTouchMove: false };
+  registerFlowForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    hostCity: [''],
+    hostCountry: [''],
+    homeCountry: ['']
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   nextSlide(): void {
     this.slides.slideNext();
