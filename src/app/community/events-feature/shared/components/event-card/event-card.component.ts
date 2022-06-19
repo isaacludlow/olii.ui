03cda5d2f152474c3c3d6830b89c06f2e530d562
Isaac Ludlow <isaac@theludlows.org>
2022-06-19 16:00:42 -0600

    [DLR-25] Working on the event card.

---
+++ src/app/community/events-feature/shared/components/event-card/event-card.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Event } from 'src/app/models/dto/community/events/event.dto';
+
+@Component({
+  selector: 'olii-event-card',
+  template: `
+    <ion-card>
+      <div class="content">
+        <olii-responsive-aspect-ratio-container aspectRatio="1/1">
+          <olii-container-cover-image [imageUrl]="event.CoverImageUrl" boarderRadius="5%"></olii-container-cover-image>
+        </olii-responsive-aspect-ratio-container>
+        <div class="event-details">
+          <h5>{{ event.Title }}</h5>
+          <div class="location-time">
+            <olii-location-preview [locationText]=""></olii-location-preview>
+            <olii-date-time-preview [date]="event.Date"></olii-date-time-preview>
+          </div>
+          <olii-profile-preview-icons [profilePictureUrls]="" profileIconSize="small"></olii-profile-preview-icons>
+        </div>
+      </div>
+    </ion-card>
+  `,
+  styleUrls: ['./event-card.component.scss']
+})
+export class EventCardComponent implements OnInit {
+  @Input() event: Event;
+  profilePictureUrls: Array<string>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.profilePictureUrls = this.event.theImageUrls for the people attending
+  }
+
+}
--- src/app/components/shared/profile-preview-icons/profile-preview-icons.component.ts
+++ src/app/components/shared/profile-preview-icons/profile-preview-icons.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'olii-profile-preview-icons',
   template: `
     <div class="profile-icons-container">
-      <img class="profile-images" [ngClass]="profileIconSize" *ngFor="let profileImageUrl of profileImageUrls" [src]="profileImageUrl">
+      <img class="profile-images" [ngClass]="profileIconSize" *ngFor="let profileImageUrl of profilePictureUrls" [src]="profileImageUrl">
     </div>
   `,
   styleUrls: ['./profile-preview-icons.component.scss']
 })
 export class ProfilePreviewIconsComponent {
-  @Input() profileImageUrls: Array<string>;
+  @Input() profilePictureUrls: Array<string>;
   @Input() profileIconSize: string;
+  // @Input() additionDisplayNumber: number = null; // TODO: Show and hide the addition number to display next to the profile preview icons.
 }
