0a42d66aae42ad9cf4c5f07e81aa37170ac1c653
Isaac Ludlow <isaac@theludlows.org>
2022-08-16 17:06:36 -0600

    [DLR-47] Initial changes. Need to fix errors.

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -6,6 +6,7 @@ import { UserRequest } from 'src/app/models/requests/user/user-request';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthStore } from '../authentication/auth-store';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,17 @@ export class UserService {
   constructor(private httpClient: HttpClient, private authStore: AuthStore) {}
 
   getUserByUid(uid: string): Observable<User> {
-    const response = this.httpClient.get<User>(`${environment.apiBaseUrl}/user`, { headers: { Authorization: this.authStore.userIdToken.value } });
+    const response = this.httpClient.get<User>(`${environment.apiBaseUrl}/user`, { headers: { Authorization: this.authStore.userIdToken.value } }).pipe(
+      tap(user => user.Id = 98)
+    );
 
     return response;
   }
 
   createUser(user: UserRequest): Observable<User> {
-    const response = this.httpClient.post<User>(`${environment.apiBaseUrl}/user`, user, { headers: { Authorization: this.authStore.userIdToken.value } });
+    const response = this.httpClient.post<User>(`${environment.apiBaseUrl}/user`, user, { headers: { Authorization: this.authStore.userIdToken.value } }).pipe(
+      tap(user => user.Id = 98)
+    );
 
     return response;
   }
