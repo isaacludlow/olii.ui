41761312101d901ab5951524e7ff3b0c5a4995b3
Isaac Ludlow <isaac.ludlow@verbinapp.org>
2022-10-25 08:09:45 -0600

    [DLR-60] Error handling.

--- src/app/shared/services/bankend/cloud-storage-service/cloud-storage.service.ts
+++ src/app/shared/services/bankend/cloud-storage-service/cloud-storage.service.ts
@@ -11,8 +11,21 @@ export class CloudStorageService {
   uploadFile(base64EncodedString: string, cloudFilePath: string): { UploadProgressPercentage$: Observable<number>, DownloadUrl$: Observable<string> } {
     const filePathRef = this.storageService.ref(cloudFilePath);
     const task = filePathRef.putString(base64EncodedString, 'data_url');
-    const uploadPercentage = task.percentageChanges();
+    task.catch(error => {
+      switch (error.name) {
+        case 'storage/unauthenticated':
+          alert('User is unauthenticated, please login and try again.');
+          break;
+        case 'storage/unauthorized':
+          alert('You do not have permission to perform this action');
+          break;
+        default:
+          alert('There was an error. Please check your input and try again. If the problem persists, please contact us through our website.')
+          break;
+      }
+    });
 
+    const uploadPercentage = task.percentageChanges();
     const downloadUrl = filePathRef.getDownloadURL();
 
     return { UploadProgressPercentage$: uploadPercentage, DownloadUrl$: downloadUrl };
