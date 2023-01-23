b3b99ed997957309f51868b8d988ee8c398d97e0
Isaac Ludlow <isaac.ludlow@verbinapp.org>
2023-01-23 07:43:20 -0700

    [DLR-BugFixes] Fixed event card image.

--- src/app/components/shared/date-time-preview/date-time-preview.component.ts
+++ src/app/components/shared/date-time-preview/date-time-preview.component.ts
@@ -3,13 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'olii-date-time-preview',
   template: `
-    <div class="container">
-        <div class="date-container">
-          <olii-colored-square-background backgroundColor="translucent-purple">
-            <olii-base-icon size="extra-small" name="time-outline" color="primary"></olii-base-icon>
-          </olii-colored-square-background>
-          <div class="font-body-s line-clamp">{{ date | date: 'EE, MMM d' }} {{ date | date: 'h:mm a' }}</div>
-      </div>
+    <div class="date-container">
+      <olii-colored-square-background backgroundColor="translucent-purple">
+        <olii-base-icon size="mini" name="time-outline" color="primary"></olii-base-icon>
+      </olii-colored-square-background>
+      <div class="font-body-s line-clamp">{{ date | date: 'EE, MMM d' }} {{ date | date: 'h:mmaaaaa' }}</div>
     </div>
   `,
   styleUrls: ['./date-time-preview.component.scss']
