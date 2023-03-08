8613e86d95002e6737c247a6da69e88b8925d4bb
Isaac Ludlow <isaac.ludlow@verbinapp.org>
2023-03-08 06:58:35 -0700

    [DLR-83] Removing code.

--- src/app/shared/services/community/events-feature/events-feature.store.ts
+++ src/app/shared/services/community/events-feature/events-feature.store.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { isAfter, isBefore } from 'date-fns';
-import { combineLatest, from, merge, Observable, zip } from 'rxjs';
-import { map, mergeMap, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
+import { combineLatest, from, Observable, zip } from 'rxjs';
+import { map, shareReplay, startWith, switchMap } from 'rxjs/operators';
 import { Event } from 'src/app/models/dto/community/events/event.dto';
 import { ProfilePreview } from 'src/app/models/dto/profile/profile-preview.dto';
 import { CloudStorageService } from '../../bankend/cloud-storage-service/cloud-storage.service';
@@ -41,10 +40,6 @@ export class EventsFeatureStore {
     let event = combineLatest(eventObservables).pipe(
       map(events => events.flat().find(event => event.EventId === eventId))
     );
-
-    if (!event) {
-      event = this.dbService.getEventById(eventId);
-    }
 
     return event;
   }
