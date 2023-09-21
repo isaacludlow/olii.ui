5b220afb3ce1319b77f78828a72fad8062f160ee
Isaac Ludlow <31048740+isaacludlow@users.noreply.github.com>
2023-09-21 15:09:31 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   useEmulators: false,
   production: true,
-  firebase: {
-    apiKey: "AIzaSyCpPv2UX3NOQFB8Y8NwfM7dvVi4Nn-nzQc",
-    authDomain: "oliiapp.firebaseapp.com",
-    projectId: "oliiapp",
-    storageBucket: "oliiapp.appspot.com",
-    messagingSenderId: "1010655614462",
-    appId: "1:1010655614462:web:b57f8845a4a707cac69caf",
-    measurementId: "G-R6KP5B8T29"
-  }
 };
